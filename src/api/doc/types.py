3e009c0e1377dca705b88017df4a4a24f341cb6c
MamoruDS <mamoruds.io@gmail.com>
2024-07-12 23:30:57 +0900

    update: annotations of types for serde
    Update CARLA API Doc types' annotations to align with CARLA 0.9.14 format.

--- src/api/doc/types.py
+++ src/api/doc/types.py
@@ -1,9 +1,12 @@
-from serde import serde
+from typing import Any
+
+from serde import serde, field
 
 
 @serde
 class DocClsInstanceVar:
     var_name: str
+    units: str | None = field(alias=["param_units", "var_units"])
     type: str | None = None
     doc: str | None = None
 
@@ -11,21 +14,30 @@ class DocClsInstanceVar:
 @serde
 class DocClsMethodParam:
     param_name: str
-    type: str
+    units: str | None = field(alias=["param_units", "var_units"])
+    type: str | None = None
+    default: Any | None = None  # TODO:
     doc: str | None = None
 
 
 @serde
 class DocClsMethod:
     def_name: str
+    return_type: str | None = field(alias=["return", "Return"])
+    static: bool | None = None
     params: list[DocClsMethodParam] | None = None
+    return_units: str | None = None
+    raises: str | None = None
     doc: str | None = None
+    note: str | None = None
+    warning: str | None = None
 
 
 @serde
 class DocClass:
     class_name: str
     doc: str
+    parent: str | None = None
     instance_variables: list[DocClsInstanceVar] | None = None
     methods: list[DocClsMethod] | None = None
 
@@ -34,3 +46,4 @@ class DocClass:
 class DocModule:
     module_name: str
     classes: list[DocClass]
+    doc: str | None = field(skip=True)

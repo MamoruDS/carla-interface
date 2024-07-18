f0a5f216ec48f5b0aadc44f261e0b0e3bb8833b7
MamoruDS <mamoruds.io@gmail.com>
2024-07-18 20:45:59 +0900

    feat: implement `ImportAlt`
    Add implementation of `ImportAlt` to help solve exports/imports based on rules.

---
+++ src/carla_doc/module/import_alt.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+from . import types as t
+
+
+@dataclass
+class ImportAlt(t.ImportAlt):
+    module: t.ModuleWrapper
+    filter_rules: t.GetNamesRules | None = None
+    filter_names: list[t.Identifier] | None = None
+    import_all: bool = False
+
+    def get_imported_namespace(self) -> t.NamespaceDict:
+        namespace = {}
+        exports = self.module.exports()
+        if self.import_all:
+            if self.filter_names is not None or self.filter_rules is not None:
+                # TODO: log warn skip
+                ...
+            namespace.update(exports)
+        else:
+            if self.filter_rules is not None:
+                exports = {
+                    n: (m, r) for n, (m, r) in exports.items() if r & self.filter_rules
+                }
+            if self.filter_names is not None:
+                exports = {n: e for n, e in exports.items() if n in self.filter_names}
+            namespace.update(exports)
+        return namespace
+
+    def to_pybind11_import(self, import_from: t.ModuleWrapper) -> t.Import:
+        mp = import_from.tree.relative(self.module.tree)
+        if self.import_all:
+            return mp.imports(all=True)
+        else:
+            return mp.imports(names=self.get_imported_namespace().keys())

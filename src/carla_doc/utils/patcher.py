29a82a3945210a437b043a5db48f31dfa6f6e4a3
MamoruDS <mamoruds.io@gmail.com>
2024-07-24 21:50:36 +0900

    improve: `DocPatch` handle more granular actions
    Add action to remove entire node with `_PATCH_REMOVE_NODE`.
    Add action to replace specific fields with `_PATCH_REPLACE_FIELDS`.
    Add action to replace entire node with `_PATCH_REPLACE_NODE`.

--- src/carla_doc/utils/patcher.py
+++ src/carla_doc/utils/patcher.py
@@ -4,13 +4,21 @@ T = TypeVar("T", bound=dict[str, Any])
 
 
 class DocPatch:
+    KEY_ACTION_REMOVE_NODE = "_PATCH_REMOVE_NODE"
+    KEY_ACTION_REPLACE_NODE = "_PATCH_REPLACE_NODE"
+    KEY_ACTION_REPLACE_FIELDS = "_PATCH_REPLACE_FIELDS"
+
     key_fields: dict[str, str]
 
     def __init__(self, key_fields):
         self.key_fields = key_fields
 
     def patch(self, origin: T, patch: T):
+        replace_fields = patch.pop(self.KEY_ACTION_REPLACE_FIELDS, None)
         for field, val in patch.items():
+            if replace_fields is not None:
+                if field in replace_fields:
+                    origin[field] = val
             if field in self.key_fields:
                 if isinstance(val, dict):
                     self.patch(origin[field], val)
@@ -32,7 +40,17 @@ class DocPatch:
     ):
         item_map = {item[key_field]: item for item in origin}
         for item in patch:
-            if item[key_field] in item_map:
-                self.patch(item_map[item[key_field]], item)
+            target = item[key_field]
+            if target in item_map:
+                if item.get(self.KEY_ACTION_REMOVE_NODE, False):
+                    item.pop(self.KEY_ACTION_REMOVE_NODE)
+                    origin.remove(item_map.pop(target))
+                elif item.get(self.KEY_ACTION_REPLACE_NODE, False):
+                    item.pop(self.KEY_ACTION_REPLACE_NODE)
+                    idx = origin.index(item_map[target])
+                    origin[idx] = item
+                    item_map[target] = item
+                else:
+                    self.patch(item_map[target], item)
             elif allow_append:
                 origin.append(item)

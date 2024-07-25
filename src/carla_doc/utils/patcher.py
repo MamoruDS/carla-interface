74ffc763af1c8121a3bc570868bba864b813e80e
MamoruDS <mamoruds.io@gmail.com>
2024-07-25 13:47:45 +0900

    improve: logging for patcher

--- src/carla_doc/utils/patcher.py
+++ src/carla_doc/utils/patcher.py
@@ -1,5 +1,9 @@
 from typing import Any, TypeVar
 
+from ..utils.logging import get_logger
+
+log = get_logger(__name__)
+
 T = TypeVar("T", bound=dict[str, Any])
 
 
@@ -14,22 +18,40 @@ class DocPatch:
     def __init__(self, key_fields):
         self.key_fields = key_fields
 
+    def _decide_key_field(self, patch: T) -> str:
+        for field in patch.keys():
+            if field in self.key_fields.values():
+                return field
+        log.error(
+            "failed to decide the key_field of patch\n"
+            + f' > patch.keys: {",".join(patch.keys())}'
+            + "   non is defined as key_fields for the patcher"
+        )
+        raise NotImplementedError()
+
     def patch(self, origin: T, patch: T):
+        key_field = self._decide_key_field(patch)
         replace_fields = patch.pop(self.KEY_ACTION_REPLACE_FIELDS, None)
         for field, val in patch.items():
             if replace_fields is not None:
                 if field in replace_fields:
+                    log.debug(
+                        "patching(REPLACE FIELDS) "
+                        + f'{type(origin)}.{field} with value "{val}"'
+                    )
                     origin[field] = val
-            if field in self.key_fields:
+            if field == key_field:
+                # skip
+                ...
+            elif field in self.key_fields:
                 if isinstance(val, dict):
                     self.patch(origin[field], val)
                 elif isinstance(val, list):
                     self.patch_list(origin[field], val, self.key_fields[field])
                 else:
                     raise NotImplementedError("unknown field:", field)
-            elif field in self.key_fields.values():
-                ...
             else:
+                log.debug(f'patching {type(origin)}.{field} with value "{val}"')
                 origin[field] = val
 
     def patch_list(
@@ -46,13 +68,29 @@ class DocPatch:
             if target in item_map:
                 if item.get(self.KEY_ACTION_REMOVE_NODE, False):
                     item.pop(self.KEY_ACTION_REMOVE_NODE)
+                    log.debug(
+                        "patching(REMOVE NODE) " + f'{type(origin)} with node "{item}"'
+                    )
                     origin.remove(item_map.pop(target))
                 elif item.get(self.KEY_ACTION_REPLACE_NODE, False):
                     item.pop(self.KEY_ACTION_REPLACE_NODE)
                     idx = origin.index(item_map[target])
+                    log.debug(
+                        "patching(REPLACE NODE) "
+                        + f'{type(origin)}[{idx}] with node "{item}"'
+                    )
                     origin[idx] = item
                     item_map[target] = item
                 else:
                     self.patch(item_map[target], item)
             elif allow_append:
+                log.debug(
+                    "patching(APPEND NODE) " + f'{type(origin)} with node "{item}"'
+                )
                 origin.append(item)
+            else:
+                log.error(
+                    "panic during patching!\n"
+                    + f' > target "{target}" cannot be found with key field <{key_field}>'
+                )
+                raise NotImplementedError()

ce630ff7d8d5f4ac6314b07425dc47994bb79739
MamoruDS <mamoruds.io@gmail.com>
2024-07-25 20:59:23 +0900

    update: point target to `dist/carla`
    Update `tool.hatch.build.targets.wheel.packages` to `dist/carla` to contain built stub files.

--- src/carla/_carla/_c/rpc/__init__.pyi
+++
@@ -1,5 +0,0 @@
-# https://github.com/carla-simulator/carla/blob/master/LibCarla/source/carla/rpc/ActorId.h
-ActorId = int
-
-# https://github.com/carla-simulator/carla/blob/master/LibCarla/source/carla/rpc/LightState.h
-LightId = int
--- src/carla/_carla/_c/sensor/data/__init__.py
+++
@@ -1,6 +0,0 @@
-# https://github.com/carla-simulator/carla/blob/master/LibCarla/source/carla/sensor/data/Color.h
-class Color:
-    def __init__(
-        self, r: int = 0, g: int = 0, b: int = 0, a: int = 255
-    ) -> None:
-        ...

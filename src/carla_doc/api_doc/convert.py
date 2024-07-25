9a9915b6d8f8717929aff4294aaec429d891d44a
MamoruDS <mamoruds.io@gmail.com>
2024-07-25 12:41:06 +0900

    add: methods for converting doc_type with pybind11_stubgen structs
    Add helper method `is_enum` to determine if an API `DocClass` is enum based.
    Add convert method `cvt_enum` to convert `DocClass` into enum-based `structs.Class`.
    Add convert method `cvt_field` to convert `DocClsInstanceVar` into `structs.Field`.

--- src/carla_doc/api_doc/convert.py
+++ src/carla_doc/api_doc/convert.py
@@ -21,11 +21,61 @@ class Convertor:
         return structs.Argument(structs.Identifier("self"))
 
     @staticmethod
+    def is_enum(doc_cls: dt.DocClass) -> bool:
+        if doc_cls.parent is not None:
+            return False
+        if doc_cls.methods is not None and len(doc_cls.methods) > 0:
+            return False
+        if doc_cls.instance_variables is None or len(doc_cls.instance_variables) == 0:
+            return False
+        else:
+            for var in doc_cls.instance_variables:
+                if not var.var_name[0].isupper():
+                    return False
+                if var.type is not None:
+                    return False
+        return True
+
+    @staticmethod
     def cvt_docstr(doc: str | None) -> structs.Docstring | None:
         if doc is not None:
             return structs.Docstring(doc)
         else:
             return None
+
+    @classmethod
+    def cvt_enum(cls, target: dt.DocClass, tc: TypeResolver) -> structs.Class:
+        fields: list[structs.Field] = []
+        for var in target.instance_variables or []:
+            if var.var_name == "None":
+                var.var_name = "NONE"
+            f = cls.cvt_field(var, tc)
+            f.attribute.value = structs.Value("auto()", is_print_safe=True)
+            fields.append(f)
+        # FIXME:
+        tc.cvt_type("enum.IntEnum", tc)
+        tc.cvt_type("enum.auto", tc)
+        return structs.Class(
+            structs.Identifier(target.class_name),
+            doc=cls.cvt_docstr(target.doc),
+            bases=[structs.QualifiedName.from_str("IntEnum")],
+            fields=fields,
+        )
+
+    @classmethod
+    def cvt_field(cls, target: dt.DocClsInstanceVar, tc: TypeResolver) -> structs.Field:
+        f_type = None
+        if target.type is not None:
+            f_type = tc.cvt_type(target.type, tc)
+        # TODO: structs.Field doesn't come with doc
+        return structs.Field(
+            structs.Attribute(
+                structs.Identifier(target.var_name),
+                None,
+                f_type,
+            ),
+            None,
+        )
 
     @classmethod
     def cvt_property(

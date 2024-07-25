8967236713b42db02a7f03a8943d7eeb7ab6cceb
MamoruDS <mamoruds.io@gmail.com>
2024-07-25 12:28:01 +0900

    add: method for converting `DocClsInstanceVar` to `t.Property`

--- src/carla_doc/api_doc/convert.py
+++ src/carla_doc/api_doc/convert.py
@@ -1,4 +1,16 @@
+from enum import IntFlag, auto
+
 from pybind11_stubgen import structs
+
+from ..module.type_resolver import TypeResolver
+from . import types as dt
+
+
+class PropertyFns(IntFlag):
+    NONE = 0
+    FDEL = auto()
+    FGET = auto()
+    FSET = auto()
 
 
 class Convertor:
@@ -7,3 +19,52 @@ class Convertor:
     @staticmethod
     def arg_self() -> structs.Argument:
         return structs.Argument(structs.Identifier("self"))
+
+    @staticmethod
+    def cvt_docstr(doc: str | None) -> structs.Docstring | None:
+        if doc is not None:
+            return structs.Docstring(doc)
+        else:
+            return None
+
+    @classmethod
+    def cvt_property(
+        cls,
+        target: dt.DocClsInstanceVar,
+        tc: TypeResolver,
+        *,
+        flag: PropertyFns = PropertyFns.FGET,
+    ) -> structs.Property:
+        prop_type, getter, setter = None, None, None
+        doc = cls.cvt_docstr(target.doc)
+        if target.type is not None:
+            prop_type = tc.cvt_type(target.type, tc)
+        # TODO:
+        # if PropertyFns.FDEL & flag:
+        if PropertyFns.FGET & flag:
+            getter = structs.Function(
+                structs.Identifier(target.var_name),
+                args=[cls.arg_self()],
+                returns=prop_type,
+                doc=doc,
+            )
+        if PropertyFns.FSET & flag:
+            setter = structs.Function(
+                structs.Identifier(target.var_name),
+                args=[
+                    cls.arg_self(),
+                    structs.Argument(
+                        structs.Identifier("val"),
+                        annotation=prop_type,
+                    ),
+                ],
+                returns=None,
+                doc=doc,
+            )
+        return structs.Property(
+            structs.Identifier(target.var_name),
+            modifier=None,
+            doc=doc,
+            getter=getter,
+            setter=setter,
+        )

fa6f9419620c39387fa4c6dfb59cf08b444f18c6
MamoruDS <mamoruds.io@gmail.com>
2024-07-25 16:32:12 +0900

    add: CLI arguments

---
+++ src/carla_doc/cli.py
@@ -0,0 +1,14 @@
+from .utils.logging import LogLevel
+
+
+class Args:
+    # @alias=i
+    input_root: str
+
+    patches_root: str
+
+    # @alias=o
+    output_root: str
+
+    # @alias=l
+    level: LogLevel = LogLevel.WARNING

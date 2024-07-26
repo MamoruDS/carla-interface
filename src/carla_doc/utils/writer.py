a813b6a14dca122b80ba7394e2e7ee8b4f7d3fca
MamoruDS <mamoruds.io@gmail.com>
2024-07-26 22:33:21 +0900

    update: write generated files to with stubs suffix
    Refactor writer with new attribute/ctor param `package_name` to overwrite root module write directory.

--- src/carla_doc/utils/writer.py
+++ src/carla_doc/utils/writer.py
@@ -9,15 +9,18 @@ log = get_logger(__name__)
 
 
 class Writer:
+    package_name: str
     _printer: Printer
 
-    def __init__(self, printer: Printer | None = None) -> None:
+    def __init__(self, package_name: str, printer: Printer | None = None) -> None:
+        self.package_name = package_name
         self._printer = printer or Printer(invalid_expr_as_ellipses=True)
 
     def write_to_files(self, module: ModuleWrapper, root: Path):
         mod = module.get_print_ready_module()
+        name = mod.name if module.tree.root() is not module.tree else self.package_name
         if len(module.tree.children):
-            mod_root = root / mod.name
+            mod_root = root / name
             mod_root.mkdir()
             mod_file = mod_root / "__init__.pyi"
             with mod_file.open("w") as f:
@@ -28,7 +31,7 @@ class Writer:
                 self.write_to_files(child, mod_root)
 
         else:
-            mod_file = root / f"{mod.name}.pyi"
+            mod_file = root / f"{name}.pyi"
             with mod_file.open("w") as f:
                 log.info(f"writing module <{module}> to file {mod_file} ...")
                 f.writelines(ln + "\n" for ln in self._printer.print_module(mod))
